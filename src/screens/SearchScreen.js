12011ad1c56c978d67f63d5e4c1faef0c22bd223
Xiaoheng Pan <xiaoheng.pan@tinroofsoftware.com>
2020-11-03 15:02:12 -0500

    initial setup

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,31 @@
+// import React from 'react';
+// import { View, Text, StyleShet } from 'react-native';
+
+// function SearchScreen(props) {
+//     return (
+//         <View>
+//             <Text>Search Screen </Text>
+//         </View>
+//     );
+// }
+
+// const styles = StyleSheet.create({
+    
+// })
+
+// export default SearchScreen;
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Search Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
